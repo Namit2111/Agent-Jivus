5bce1539c7dd811dc78d5718edbad6d539c205bb
Nmait2111 <namitjain2111@gmail.com>
2024-11-16 21:22:56 +0530

    groq api pipeline

--- RAG/ai_groq.py
+++ RAG/ai_groq.py
@@ -4,21 +4,10 @@ from datetime import datetime
 from groq import Groq
 
 groq = Groq(
-    api_key="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",
+    api_key="",
 )
-
-# now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-# persona = "Teemu"
-# home_location = "Madrid"
-schema = memory_schema = {
-  "type": "object",
-  "properties": {
-    "query_answer": {
-      "type": "string",
-      "description": "This will be the answer to the user query"
-    },
-    
-  }
+schema  = {
+  "type":"string"
 }
 prompt_by_user = "Today was sunny day and then rained, I went to city to have a dinner with friends and I ate the best Sushi I have ever tested in restaurant called Sushita Cafe, where my friend Paco is a chef."
 
@@ -26,16 +15,15 @@ chat_completion = groq.chat.completions.create(
     messages=[
         {
             "role": "system",
-            "content": f"You are helpful memory recorder.\nWrite outputs in JSON in schema: {schema}.\nCurrent time is {now}.\nI am {persona} living in {home_location} and events may take place in more specific places inside the home location or outside it, so record precisely.\n",
-            #"content": "You are helpful memory recorder. Write outputs in JSON schema.\n",
-            #f" The JSON object must use the schema: {json.dumps(my_schema.model_json_schema(), indent=1)}",
+            "content": f"you are a categorizing agent ,you will categorize the user query in one of three types 1) product_case_study 2) asking_for_product 3) other , return a json with key being type and balue being the type of query",
+          
         },
         {
             "role": "user",
-            "content": "Today was sunny day and then rained, I went to city to have a dinner with friends and I ate the best Sushi I have ever tested in restaurant called Sushita Cafe, where my friend Paco is a chef.",
+            "content": "what is the product name",
         }
     ],
-    model="llama-3.2-1b-preview",
+    model="llama-3.2-3b-preview",
     response_format={"type": "json_object"},
 )
 print(chat_completion.choices[0].message.content)
